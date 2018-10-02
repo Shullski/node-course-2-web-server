6435ae108afe49a4a88c9ba37ed6556a0dd2f26c
Mark Shull <shullski@MacBook-Pro-6.local>
2018-10-02 16:09:54 -0400

    Added a start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+var port = process.env.PORT || 3000;
 var app = express();
 
 // middleware ---
@@ -64,7 +65,7 @@ app.get('/bad', (req, res) => {
 
 
 // TERMINAL NOTIFICATION  --------------
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
 //--------------------------------------
